00fda3a3e655880f6a0af88d87ad4d32b514f342
Carlos Gizbert <carlos.gizbert@zapsign.com.br>
2024-07-13 20:10:24 -0300

    refactor(tasks)

--- stocks/tasks.py
+++ stocks/tasks.py
@@ -1,10 +1,12 @@
 from celery import shared_task
 from django.utils import timezone
+from datetime import timedelta
+import yfinance as yf
 
 from prostocks.utils.email_utils import send_email
 from .models import Stock, PriceEntry
-import yfinance as yf
-from datetime import timedelta
+
+RANGE_TO_NOTIFY_IN_MINUTES = 120
 
 @shared_task
 def send_email_task(subject, body, to_email):
@@ -17,47 +19,65 @@ def update_prices_periodically():
 
     for stock in stocks:
         if current_minute % stock.check_frequency == 0:
-            ticker = yf.Ticker(stock.symbol)
-            today = timezone.localdate()
-            current_time = timezone.localtime(timezone.now())
-            price_data = ticker.history(interval='1m', period='1d')
+            update_stock_price(stock)
 
-            if not price_data.empty:
-                latest_data = price_data.iloc[-1]
-                open_price = latest_data['Open']
-                close_price = latest_data['Close']
-                high_price = latest_data['High']
-                low_price = latest_data['Low']
+def update_stock_price(stock):
+    ticker = yf.Ticker(stock.symbol)
+    price_data = ticker.history(interval='1m', period='1d')
 
-                new_entry = PriceEntry(
-                    stock=stock,
-                    date=today,
-                    time=current_time.time(),
-                    open_price=open_price,
-                    close_price=close_price,
-                    high_price=high_price,
-                    low_price=low_price
-                )
-                new_entry.save()
+    if not price_data.empty:
+        latest_data = price_data.iloc[-1]
+        open_price = latest_data['Open']
+        close_price = latest_data['Close']
+        high_price = latest_data['High']
+        low_price = latest_data['Low']
+        current_time = timezone.localtime(timezone.now())
 
-                RANGE_TO_NOTIFY_IN_MINUTES = 120
+        new_entry = PriceEntry(
+            stock=stock,
+            date=timezone.localdate(),
+            time=current_time.time(),
+            open_price=open_price,
+            close_price=close_price,
+            high_price=high_price,
+            low_price=low_price
+        )
+        new_entry.save()
 
-                if close_price < stock.lower_tunnel_limit and (
-                        not stock.last_notification_sent
-                        or stock.last_notification_sent < timezone.now() - timedelta(minutes=RANGE_TO_NOTIFY_IN_MINUTES)
-                ):
-                    subject = f"ProStocks - Sugestão de compra para {stock.symbol}"
-                    html = f"<strong>O preço de {stock.symbol} é R${high_price}, e cruzou o limite inferior do túnel. Sugerimos avaliar uma compra. <br> Caso o valor se mantenha, você receberá uma nova notificação em {RANGE_TO_NOTIFY_IN_MINUTES} minutos</strong>"
-                    send_email(subject, html_content=html, text_content='Teste', to_email=stock.user.username)
-                    stock.last_notification_sent = timezone.now()
-                    stock.save()
+        check_and_send_notification(stock, close_price, high_price)
 
-                elif close_price > stock.upper_tunnel_limit and (
-                        not stock.last_notification_sent
-                        or stock.last_notification_sent < timezone.now() - timedelta(minutes=RANGE_TO_NOTIFY_IN_MINUTES)
-                ):
-                    subject = f"ProStocks - Sugestão de venda para {stock.symbol}"
-                    html = f"<strong>O preço de {stock.symbol} é é R${high_price}, e cruzou o limite superior do túnel. Sugerimos avaliar uma venda. <br> Caso o valor se mantenha, você receberá uma nova notificação em {RANGE_TO_NOTIFY_IN_MINUTES} minutos</strong>"
-                    send_email(subject, html_content=html, text_content='Teste', to_email=stock.user.username)
-                    stock.last_notification_sent = timezone.now()
-                    stock.save()
+def check_and_send_notification(stock, close_price, high_price):
+    if should_notify(stock, close_price, stock.lower_tunnel_limit):
+        send_notification(
+            stock, close_price, high_price, "compra",
+            "cruzou o limite inferior do túnel. Sugerimos avaliar uma compra."
+        )
+        stock.last_notification_sent = timezone.now()
+        stock.save()
+
+    elif should_notify(stock, close_price, stock.upper_tunnel_limit):
+        send_notification(
+            stock, close_price, high_price, "venda",
+            "cruzou o limite superior do túnel. Sugerimos avaliar uma venda."
+        )
+        stock.last_notification_sent = timezone.now()
+        stock.save()
+
+def should_notify(stock, close_price, tunnel_limit):
+    return close_price < tunnel_limit and (
+        not stock.last_notification_sent
+        or stock.last_notification_sent < timezone.now() - timedelta(minutes=RANGE_TO_NOTIFY_IN_MINUTES)
+    )
+
+def send_notification(stock, close_price, high_price, action, message):
+    subject = f"ProStocks - Sugestão de {action} para {stock.symbol}"
+    html_content = (
+        f"<strong>O preço de {stock.symbol} é R${high_price}, e {message} <br>"
+        f"Caso o valor se mantenha, você receberá uma nova notificação em {RANGE_TO_NOTIFY_IN_MINUTES} minutos</strong>"
+    )
+    send_email(
+        subject,
+        html_content=html_content,
+        text_content='Teste',
+        to_email=stock.user.username
+    )
